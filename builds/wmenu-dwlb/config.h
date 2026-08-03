57d75569fdd2f2676e0d48836b0c42ba176de3dc
caos-obliquo <caos_obliquo@proton.me>
2026-08-03 13:52:49 -0300

    refactor: symlink builds configs to live ~/builds sources

--- builds/wmenu-dwlb/config.h
+++ builds/wmenu-dwlb/config.h
@@ -1,6 +1,1 @@
-/* wmenu-dwlb configuration - matches dwlb active monitor colors */
-
-/* bar mode (-t) colors */
-static const unsigned int dwlb_middle_bg  = 0xbd93f9a6; /* Dracula purple */
-static const unsigned int dwlb_middle_fg  = 0xf8f8f2ff; /* Foreground */
-static const unsigned int dwlb_bar_height = 30;
+../../../builds/wmenu-dwlb/config.h
